9bf3fd069905266dbc85b78914fdb56c70fa51fa
James Haydon <james.haydon@gmail.com>
2015-05-11 19:05:37 +0900

    init from h5bp

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('#slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 250,
+    slidesToShow: 1,
+    fade: true,
+    ccsEase: 'linear',
+    adaptiveHeight: true,
+    autoplay: true
+  });
+});
